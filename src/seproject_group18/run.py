a906377262b0ac527bd5e01cc566e91145c4b55c
Peng Ye <peng.ye@uceconnect.ie>
2018-03-12 13:44:11 +0000

    2018.03.12 add friendly UI on command

--- src/seproject_group18/run.py
+++ src/seproject_group18/run.py
@@ -1,24 +1,104 @@
-import os 
+import os, sys, time, subprocess
+import logging, threading
 from seproject_group18.app import app
-from seproject_group18.script import bikeinfo, weatherinfo 
+from seproject_group18.script import bikeinfo, weatherinfo, processmanagement 
 from multiprocessing import Process
 
-def runInParallel(*fns):
-  proc = []
-  for fn in fns:
-    p = Process(target=fn)
-    p.start()
-    proc.append(p)
-  for p in proc:
-    p.join()
+class StreamToLogger(object):
+   """
+   Fake file-like stream object that redirects writes to a logger instance.
+   """
+   def __init__(self, logger, log_level=logging.INFO):
+      self.logger = logger
+      self.log_level = log_level
+      self.linebuf = ''
 
-def webserver():
+   def write(self, buf):
+      for line in buf.rstrip().splitlines():
+         self.logger.log(self.log_level, line.rstrip())
+
+class WebServer(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
+
+    def __init__(self, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        """ Method that runs forever """
+        '''
+        app.run(host='0.0.0.0', port=5000)
+        '''
+        while True:
+            # Do something
+            print('Doing something imporant in the background')
+
+            time.sleep(self.interval)
+
+
+def runInParallel(*fns):
+    proc = []
+    for fn in fns:
+        p = Process(target=fn)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+
+def webserver_func():
+    print("booting web server...")
     app.run(host='0.0.0.0', port=5000)
 
 def main():
-    runInParallel(bikeinfo.main, weatherinfo.main, webserver)
-    #app.run(host='0.0.0.0', port=5000)
-    print("import module successfull")
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    stdout = current_path + "/../../log/DublinBike.log"
+    logging.basicConfig(
+       level=logging.DEBUG,
+       format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+       filename=stdout,
+       filemode='a'
+    )
+
+    if len(sys.argv) == 1:
+        print(sys.argv[0] + " {webstart|dbstart|allstart|status|stop}")
+        sys.exit(1)
+    elif sys.argv[1] == 'status':
+        my_process = processmanagement.ProcessManagement(['run.py', 'dublinbike', 'start'])
+        if my_process.isProcessRunning():
+            print("It is running now!")
+        else:
+            print("It is stopped!")
+    elif sys.argv[1] == 'stop':
+        my_process = processmanagement.ProcessManagement(['run.py', 'dublinbike', 'start'])
+        if my_process.killProcessRunning():
+            print("Stopped!")
+
+    fpid = os.fork()
+    if fpid!=0:
+        sys.exit(0)
+    if sys.argv[1] == 'webstart':
+        webserver_func()
+    elif sys.argv[1] == 'dbstart':
+        runInParallel(bikeinfo.main, weatherinfo.main)
+    elif sys.argv[1] == 'allstart':
+        runInParallel(bikeinfo.main, weatherinfo.main, webserver_func)
+    stdout_logger = logging.getLogger('STDOUT')
+    sl = StreamToLogger(stdout_logger, logging.INFO)
+    sys.stdout = sl
+
+    stderr_logger = logging.getLogger('STDERR')
+    sl = StreamToLogger(stderr_logger, logging.ERROR)
+    sys.stderr = sl
 
 if __name__ == "__main__": 
     main()
---
+++ src/seproject_group18/script/processmanagement.py
@@ -0,0 +1,47 @@
+import subprocess, os, signal, sys
+
+class ProcessManagement:
+    def __init__(self, process_name):
+        self.__process_name = process_name
+
+    def findProcess(self):
+        ps= subprocess.Popen("ps -ef | grep -e "+self.__process_name[0]+"  -e " +self.__process_name[1]+" | grep "+self.__process_name[2], shell=True, stdout=subprocess.PIPE)
+        lines = []
+        output = ps.stdout.readline().strip()
+        while(output):
+            lines.append(str(output))
+            output = ps.stdout.readline().strip()
+        ps.stdout.close()
+        ps.wait()
+        #print(lines)
+        return lines
+
+    def isProcessRunning(self):
+        output = self.findProcess()
+        if len(output) > 1:
+            return True
+        else:
+            return False
+
+    def killProcessRunning(self):
+        output = self.findProcess()
+        pids = []
+        for line in output:
+            elements = line.split()
+            if elements[1] not in pids:
+                pids.append(elements[1])
+        try:
+            for pid_str in pids:
+                os.kill(int(pid_str), signal.SIGTERM)
+                #print("killed " + pid_str)
+        except:
+            pass
+        return not self.isProcessRunning()
+
+'''
+#print(isProcessRunning('run.py'))
+# ProcessManagement([pattern1, pattern2, pattern3])
+# pattern1 or pattern2 + pattern3
+my_process = ProcessManagement(['run.py', 'run.py', 'allstart'])
+my_process.killProcessRunning()
+'''
